66e26c22cfd7a09decb9e120925dd63f5aabeb98
boyeon kim <hajoce02@naver.com>
2024-08-19 01:08:53 +0900

    6th commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import WeatherBox from "./component/WeatherBox";
 import WeatherButton from "./component/WeatherButton";
+import ClipLoader from "react-spinners/ClipLoader";
 
 // 1. 앱이 실행되자마자 현재위치 기반의 날씨가 보인다
 // 2. 날씨 정보에는 도시, 섭씨, 화씨 날씨 상태가 확인된다
@@ -14,6 +15,7 @@ import WeatherButton from "./component/WeatherButton";
 function App() {
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
   const cities = ["paris", "new york", "tokyo", "seoul"];
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -26,28 +28,62 @@ function App() {
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=310beaa8565789898bb0a57ddcd5a5d4&units=metric`;
+    setLoading(true);
     let response = await fetch(url); // 비동기
     let data = await response.json();
     console.log("data", data);
     setWeather(data);
+    setLoading(false);
+  };
+
+  const getWeatherByCity = async () => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=310beaa8565789898bb0a57ddcd5a5d4&units=metric`;
+    setLoading(true);
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log("data", data);
+    setWeather(data);
+    setLoading(false);
+  };
+
+  const handleCityChange = () => {
+    if (city === "current") {
+      setCity(null);
+    } else {
+      setCity(city);
+    }
   };
 
   useEffect(() => {
-    getCurrentLocation();
-  }, []); // array 값이 비어있어 componentDidMount()처럼 작동, render 후 바로 작동
-
-
-  useEffect(()=>{
-    console.log("city?",city)
-  },[city])
-
+    if (city === "") {
+      getCurrentLocation();
+    } else {
+      getWeatherByCity();
+    }
+  }, [city]); // array 값이 비어있으면 componentDidMount()처럼 작동, render 후 바로 작동
 
   return (
     <div>
-      <div className="container">
-        <WeatherBox weather={weather} />
-        <WeatherButton cities={cities} setCity={setCity}/>
-      </div>
+      {loading ? (
+        <div className="container">
+          <ClipLoader
+            color="#87CEEB"
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <div className="container">
+          <WeatherBox weather={weather} />
+          <WeatherButton
+            cities={cities}
+            handleCityChange={handleCityChange}
+            setCity={setCity}
+          />
+        </div>
+      )}
     </div>
   );
 }
--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
-const WeatherButton = ({ cities,setCity }) => {
+const WeatherButton = ({ cities, setCity }) => {
   console.log("cities?", cities);
   return (
     <div>
-      <Button variant="primary">Current Location</Button>{" "}
+      <Button variant="primary" onClick={() => handleCityChange("current")}>
+        Current Location
+      </Button>{" "}
       {cities.map((item, index) => (
-        <Button variant="primary" key={index} onClick={()=>setCity(item)}>{item}</Button>
+        <Button variant="primary" key={index} onClick={() => setCity(item)}>
+          {item}
+        </Button>
       ))}
     </div>
   );
