2754575efaf996e3f7247d4ee5854fafb2dba5db
boyeon kim <hajoce02@naver.com>
2024-08-19 23:32:29 +0900

    11th commit

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const WeatherBox = ({ weather }) => {
   if (!weather || !weather.main) {
-    return <div>Loading...</div>; // 또는 null을 반환하여 아무것도 렌더링하지 않음
+    return <div>Loading...</div>;
   }
 
-  const celsius = Math.round(weather.main.temp); // 소수점 이하 반올림
-  const fahrenheit = Math.round(celsius * 1.8 + 32); // 소수점 이하 반올림
+  const celsius = Math.round(weather.main.temp);
+  const fahrenheit = Math.round(celsius * 1.8 + 32);
 
   return (
     <div className="weather_box">
